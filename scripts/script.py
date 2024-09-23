53f186c663582aae8290e51896679e56508c3e5b
Airat Safin <airatsafin65578@gmail.com>
2024-09-23 15:16:09 +0300

    Продвинутый функционал для тестового задания

--- scripts/script.py
+++ scripts/script.py
@@ -1,34 +1,21 @@
 import shutil
 
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import count
+from pyspark.sql.functions import count, sum
 import sys
 import os
 from datetime import datetime, timedelta
-import subprocess
 
 spark = SparkSession.builder \
     .appName("LogAggregation") \
     .getOrCreate()
 
 
-def aggregate_logs(start_date, input_dir):
-    all_data = None
-    for i in range(7):
-        day = start_date + timedelta(days=i)
-        file_name = os.path.join(input_dir, f"{day.strftime('%Y-%m-%d')}.csv")
-        if os.path.exists(file_name):
-            data = spark.read.csv(file_name, header=False, inferSchema=True)
-            if all_data is None:
-                all_data = data
-            else:
-                all_data = all_data.union(data)
-
-    if all_data is None:
-        return None
-
+def aggregate_daily_data(date, input_dir, temp_dir):
+    file_name = os.path.join(input_dir, f"{date.strftime('%Y-%m-%d')}.csv")
+    data = spark.read.csv(file_name, header=False, inferSchema=True)
     new_columns = ["email", "action", "dt"]
-    df = all_data.toDF(*new_columns)
+    df = data.toDF(*new_columns)
     agg_df = df.groupBy("email") \
         .pivot("action", ["CREATE", "READ", "UPDATE", "DELETE"]) \
         .agg(count("*")) \
@@ -37,7 +24,44 @@ def aggregate_logs(start_date, input_dir):
         .withColumnRenamed("READ", "read_count") \
         .withColumnRenamed("UPDATE", "update_count") \
         .withColumnRenamed("DELETE", "delete_count")
-    return agg_df
+
+    output_file = os.path.join(temp_dir, f"{date.strftime('%Y-%m-%d')}.csv")
+    write_single_csv(agg_df, output_file)
+
+
+def aggregate_weekly_data(start_date, input_dir, temp_dir):
+    weekly_data = None
+
+    for i in range(7):
+        day = start_date + timedelta(days=i)
+        file_name = os.path.join(temp_dir, f"{day.strftime('%Y-%m-%d')}.csv")
+
+        if not os.path.exists(file_name):
+            aggregate_daily_data(day, input_dir, temp_dir)
+
+        data = spark.read.csv(file_name, header=True, inferSchema=True)
+
+        if weekly_data is None:
+            weekly_data = data
+        else:
+            weekly_data = weekly_data.union(data)
+
+    if weekly_data is None:
+        return None
+
+    weekly_agg = weekly_data.groupBy("email") \
+        .agg(
+        sum("create_count"),
+        sum("read_count"),
+        sum("update_count"),
+        sum("delete_count")
+        ) \
+        .withColumnRenamed("sum(create_count)", "create_count") \
+        .withColumnRenamed("sum(read_count)", "read_count") \
+        .withColumnRenamed("sum(update_count)", "update_count") \
+        .withColumnRenamed("sum(delete_count)", "delete_count")
+
+    return weekly_agg
 
 
 def write_single_csv(df, output_file):
@@ -76,7 +100,8 @@ if __name__ == "__main__":
     start_date = target_date - timedelta(days=7)
     input_dir = '/opt/airflow/input'
     output_dir = '/opt/airflow/output'
-    result = aggregate_logs(start_date, input_dir)
+    temp_dir = '/opt/airflow/temp'
+    result = aggregate_weekly_data(start_date, input_dir, temp_dir)
     if result is not None:
         output_file = os.path.join(output_dir, f"{target_date.strftime('%Y-%m-%d')}.csv")
         write_single_csv(result, output_file)
